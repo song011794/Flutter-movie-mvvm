9a9f4213e50ccab760277dea7f3e4824e547335f
song <sos9493@naver.com>
2023-06-01 19:52:39 +0900

    feature : 언어 전환 기능 추가
    1. 언어 전환 기능 추가
    
    Todo. 언어 전환 UI 구현 해야함

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_mvvm/pages/home_page.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  runApp(const ProviderScope(child: HomePage()));
+  runApp(ProviderScope(
+      child: EasyLocalization(
+          path: 'assets/translations',
+          supportedLocales: const [Locale('ko'), Locale('en')],
+          fallbackLocale: const Locale('ko'),
+          startLocale: const Locale('ko'),
+          child: Builder(builder: (context) {
+            return MaterialApp(
+                localizationsDelegates: context.localizationDelegates,
+                supportedLocales: context.supportedLocales,
+                locale: context.locale,
+                home: const HomePage());
+          }))));
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -12,47 +13,44 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
   static const List<Widget> _widgetOptions = [
     NavigationWidget(MOVIEMODE.nowPlay),
     NavigationWidget(MOVIEMODE.popular),
     NavigationWidget(MOVIEMODE.topRated),
     NavigationWidget(MOVIEMODE.upComming),
   ];
-  int _selectedIndex = 0;
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+
+  Widget _bottomNavigationBar() => BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: const FaIcon(FontAwesomeIcons.film),
+            label: tr('now_playing'),
+          ),
+          BottomNavigationBarItem(
+            icon: const FaIcon(FontAwesomeIcons.fire),
+            label: tr('popular'),
+          ),
+          BottomNavigationBarItem(
+            icon: const FaIcon(FontAwesomeIcons.star),
+            label: tr('top_rated'),
+          ),
+          BottomNavigationBarItem(
+            icon: const FaIcon(FontAwesomeIcons.stopwatch),
+            label: tr('up_comming'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) => setState(() {
+          _selectedIndex = index;
+        }),
+      );
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        home: Scaffold(
-          body: _widgetOptions.elementAt(_selectedIndex),
-          bottomNavigationBar: BottomNavigationBar(
-            items: const [
-              BottomNavigationBarItem(
-                icon: FaIcon(FontAwesomeIcons.film),
-                label: 'Now Playing',
-              ),
-              BottomNavigationBarItem(
-                icon: FaIcon(FontAwesomeIcons.fire),
-                label: 'Popular',
-              ),
-              BottomNavigationBarItem(
-                icon: FaIcon(FontAwesomeIcons.star),
-                label: 'Top Rated',
-              ),
-              BottomNavigationBarItem(
-                icon: FaIcon(FontAwesomeIcons.stopwatch),
-                label: 'Up Comming',
-              ),
-            ],
-            currentIndex: _selectedIndex,
-            selectedItemColor: Colors.black,
-            unselectedItemColor: Colors.grey,
-            onTap: _onItemTapped,
-          ),
-        ),
-      );
+  Widget build(BuildContext context) => Scaffold(
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: _bottomNavigationBar());
 }
